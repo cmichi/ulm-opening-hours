3079a70813bb243a980803624d2f1f5eeccd7566
cmichi <mich@elmueller.net>
2013-02-20 17:20:22 +0100

    still refactoring

---
+++ static/js/translations.js
@@ -0,0 +1,43 @@
+var translate = {
+	"supermarket": "Supermarkt"
+	, "bank": "Bank"
+	, "cafe": "Cafe"
+	, "bar": "Bar"
+	, "car_repair": "Autowerkstatt"
+	, "optician": "Optiker"
+	, "hairdresser": "Friseur"
+	, "shop": "Einkaufsladen"
+	, "bakery": "B&auml;cker"
+	, "travel_agency": "Reiseb&uuml;ro"
+	, "restaurant": "Restaurant"
+	, "fuel": "Tankstelle"
+	, "doctors": "&Auml;rzte"
+	, "restaurant": "Restaurant"
+	, "pharmacy": "Apotheke"
+	, "butcher": "Metzgerei"
+	, "library": "Bibliothek"
+	, "atm": "Bankautomat"
+	, "police": "Polizei"
+	, "fast_food": "Schnellimbiss"
+	, "tailor": "Schneiderei"
+	, "computer": "Computer"
+	, "hotel": "Hotel"
+	, "dancing_school": "Tanzschule"
+}
+var food = "Ernährung"
+var med = "Gesundheit"
+var buy = "Einkaufen"
+var others = "Sonstige"
+var groups = {
+	 "restaurant": 	food
+	, "supermarket": buy
+	, "bar": food
+	, "butcher": food
+	, "cafe": food
+	, "shop": buy
+	, "pharmacy": med
+	, "doctors": med
+	, "bakery": food
+	, "fast_food": food
+	, "computer": buy
+}
