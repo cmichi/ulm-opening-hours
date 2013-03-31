85f53ee5f2f326d4dc13a9a0bd06a72f46efb05b
cmichi <mich@elmueller.net>
2013-03-31 01:24:11 +0100

    added error handling for corrupt OSM entries. the exported data.xml can now be used directly by the application. entries were the parser fails are skipped.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ var express = require('express');
 var opening_hours = require('./lib/opening_hours.js');
 
 var xmlParser = require('./data/parse_xml.js')
-var data = xmlParser.getData()
+var data = xmlParser.getData();
+var parse_err_output = false;
 
 var app = express();
 app.use(express.static(__dirname + '/static'));
@@ -15,7 +16,19 @@ var server = require('http').createServer(app);
 function generateOpenEntities(date) {
 	var open_entities = [];
 	for (var i in data) {
-		var oh = new opening_hours(data[i].original_opening_hours);
+		try {
+			var oh = new opening_hours(data[i].original_opening_hours);
+		} catch (err) {			
+			/* only output error messages at the first time they are thrown.
+			   otherwise our program will flood stdout on each /get_entries request */
+			if (!parse_err_output) {
+				console.log("error while parsing '" + data[i].original_opening_hours + "':");
+				console.log(err);
+				console.log(JSON.stringify(data[i]) + "\n");
+			}
+			continue;
+		}
+		
 		var is_open = oh.getState(date);
 
 		// still open in 15 minutes?
@@ -26,6 +39,7 @@ function generateOpenEntities(date) {
 			open_entities.push(data[i]);
 		}
 	}
+	parse_err_output = true;
 
 	return open_entities;
 }
